d3d4f8fb123c316328afe1046d22d924b0365593
unknown <fbs4@cin.ufpe.br>
2021-04-07 01:06:14 -0300

    Calculator application added

---
+++ calculadora/calculadora.js
@@ -0,0 +1,188 @@
+const printar = function digitarNumero (){
+  var display = document.querySelector(".display");
+  var dentroDisplay = "";
+  dentroDisplay = display.value + this.innerHTML;
+  display.value = dentroDisplay;
+  
+  
+}
+const apagar = function apaga (){
+  if (document.querySelector(".display").value.length != 0){
+    var tamanho = document.querySelector(".display").value.length;
+    var display = document.querySelector(".display").value;
+    var resposta = "";
+    for (var x = 0; x < tamanho - 1; x++){
+      resposta = resposta + display.charAt(x);
+    }
+    
+    document.querySelector(".display").value = resposta;
+  }
+}
+const resultado = function result (){
+  var display = document.querySelector(".display").value;
+  var tamanho = document.querySelector(".display").value.length;
+  var numeroUm = "", numeroDois = "", operador = "";
+  var n1 = 0, n2 = 0, valor = 0;
+  var verifica = false, operacao = false;
+  if (this.innerHTML == "=" || this.innerHTML == "√n" || this.innerHTML == "n<sup>2</sup>"){
+    for(var x = 0; x < tamanho && verifica == false; x++){
+      if (display.charAt(x) == "+" || display.charAt(x) == "-" || display.charAt(x) == "*" || display.charAt(x) == "/"){
+        operacao = true;
+        operador = display.charAt(x);
+        console.log(operador)
+        for (var z = 0; z < x; z++){
+          if (display.charAt(z) == ","){
+            numeroUm = numeroUm + ".";
+          } else {
+            numeroUm = numeroUm + display.charAt(z);
+          }
+          n1 = parseFloat(numeroUm);
+          console.log(n1);
+        }
+        for (var y = z + 1; y < tamanho; y++){
+          if (display.charAt(y) == ","){
+            numeroDois = numeroDois + ".";
+          } else {
+            numeroDois = numeroDois + display.charAt(y);
+          }
+          n2 = parseFloat(numeroDois);
+        }
+        if (operador == "+"){
+          
+          valor = n1 + n2;
+        }
+        else if (operador == "*"){
+          
+          valor = (n1 * n2);
+        }
+        else if (operador == "-"){
+          
+          valor = (n1 - n2);
+        }
+        else if (operador == "/"){
+          
+         valor = (n1 / n2);
+        }
+        
+      }
+      
+    }
+    if (operacao == false){
+      for (var b = 0; b < tamanho; b++){
+        if (display.charAt(b) == ","){
+          numeroUm = numeroUm + ".";
+        } else {
+          numeroUm = numeroUm + display.charAt(b);
+        }
+        
+      }
+      valor = parseFloat(numeroUm);
+    }
+    verifica = true;
+    console.log(n1);
+    console.log(n2);
+    console.log(this.innerHTML);
+    if ( this.innerHTML == "√n"){
+      valor = Math.pow(valor, 0.5);
+    }
+    else if (this.innerHTML == "n<sup>2</sup>"){
+      valor = valor * valor;
+    } else {
+      valor = valor;
+    }
+    document.querySelector(".display").value = valor;
+  }
+}
+const calcular = function calcula (){
+  var display = document.querySelector(".display").value;
+  var tamanho = document.querySelector(".display").value.length;
+  var contador = 0, n1 = 0, n2 = 0, comecoN2 = 0, quandoCalcular = 0;
+  var operator = this.innerHTML;
+  var primeiroOperator = "", numeroUm = "", numeroDois = "";
+  console.log(operator)
+  for(var x = 0; x < tamanho; x++){
+    
+    if (display.charAt(x) == "+" || display.charAt(x) == "-" || display.charAt(x) == "*" || display.charAt(x) == "/"){
+      console.log("fala galera da live");
+      console.log(contador);
+      if (contador == 0){
+        primeiroOperator = display.charAt(x);
+        contador++;
+      }
+      quandoCalcular++;
+    }
+  }
+    console.log(primeiroOperator+"teste");
+    
+    if (quandoCalcular > 1){
+      for (var y = 0; y < tamanho && display.charAt(y) != primeiroOperator;y++){
+        if (display.charAt(y) == ","){
+          numeroUm = numeroUm + ".";
+        } else {
+          numeroUm = numeroUm + display.charAt(y);
+        }
+          
+          comecoN2 = y + 2;
+      }
+      console.log(numeroUm);
+      for (var z = comecoN2; z < tamanho && display.charAt(z) != this.innerHTML;z++){
+        if (display.charAt(z) == ","){
+          numeroDois = numeroDois + "."
+        } else {
+          numeroDois = numeroDois + display.charAt(z);
+        }
+          
+        
+      }
+      console.log(numeroDois);
+      if (primeiroOperator == "+"){
+        n1 = parseFloat(numeroUm);
+        n2 = parseFloat(numeroDois);
+        document.querySelector(".display").value = n1 + n2 + this.innerHTML;
+      }
+      else if (primeiroOperator == "*"){
+        n1 = parseFloat(numeroUm);
+        n2 = parseFloat(numeroDois);
+        document.querySelector(".display").value = (n1 * n2) +this.innerHTML;
+      }
+      else if (primeiroOperator == "-"){
+        n1 = parseFloat(numeroUm);
+        n2 = parseFloat(numeroDois);
+        document.querySelector(".display").value = (n1 - n2) + this.innerHTML;
+      }
+      else if (primeiroOperator == "/"){
+        n1 = parseFloat(numeroUm);
+        n2 = parseFloat(numeroDois);
+        document.querySelector(".display").value = (n1 / n2) + this.innerHTML;
+      }
+      
+    }
+  
+}
+
+const limpa = function limpar (){
+  document.querySelector(".display").value = "";
+}
+
+
+
+
+
+//-----------------------------------------------------------------------------------------------//
+
+
+
+for (var quantidadeNumeros = 0; quantidadeNumeros < 10; quantidadeNumeros++){
+    document.querySelectorAll(".numero")[quantidadeNumeros].addEventListener('click', printar); 
+}
+for (var quantidadeOperadores = 0; quantidadeOperadores < 4; quantidadeOperadores++){
+  document.querySelectorAll(".operador")[quantidadeOperadores].addEventListener('click', printar);
+  document.querySelectorAll(".operador")[quantidadeOperadores].addEventListener('click', calcular);
+}
+for (var quantidadeResult = 0; quantidadeResult < 3; quantidadeResult++){
+  document.querySelectorAll(".result")[quantidadeResult].addEventListener('click', resultado);
+}
+
+document.querySelector(".clear").addEventListener('click', limpa);
+document.querySelector(".delete").addEventListener('click', apagar)
+document.querySelector(".virgula").addEventListener('click', printar);
